730c5ffa27f1ed91e54935b72378aa03eb406a89
Brian Toro <brianhorrors@hotmail.com>
2025-10-18 22:37:56 +0200

    ignore git keeep

--- src/recognition.py
+++ src/recognition.py
@@ -6,11 +6,17 @@ from numpy.typing import NDArray
 TARGET_IMAGES_DIR = "./assets/target_images"
 FaceEmbedding = NDArray[np.float64]
 
+
 def load_image() -> FaceEmbedding:
     # load images from dir
     temp_embeddings = []
     for image_path in os.listdir(TARGET_IMAGES_DIR):
-        image = face_recognition.load_image_file(os.path.join(TARGET_IMAGES_DIR, image_path))
+        if ".gitkeep" in image_path:
+            continue
+
+        image = face_recognition.load_image_file(
+            os.path.join(TARGET_IMAGES_DIR, image_path)
+        )
         try:
             embedding = face_recognition.face_encodings(image)[0]
             temp_embeddings.append(embedding)
@@ -24,11 +30,10 @@ def load_image() -> FaceEmbedding:
     embedding_average = np.mean(temp_embeddings, axis=0)
     return np.asarray(embedding_average, dtype=np.float64)
 
+
 def locate_faces(rgb_frame, known_face_encodings, known_face_names):
     face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(
-        rgb_frame, face_locations
-    )
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
     face_names: list[str] = []
     for face_encoding in face_encodings:
